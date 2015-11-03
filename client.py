ab2a6e15d62a369e01f87a379d33d651550bb649
Eela <Eela1@Eelas-MacBook-Air.local>
2015-11-03 15:36:48 -0500

    some UDP experiments in client.py

--- client.py
+++ client.py
@@ -7,7 +7,35 @@
 
 import socket
 
+# some UDP packet sending experiments yay!
+
+UDP_IP = "127.0.0.1"
+UDP_PORT = 5005
+MESSAGE = "Hello, World!"
+
+print "UDP target IP:", UDP_IP
+print "UDP target port:", UDP_PORT
+print "message:", MESSAGE
+
+sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+
+sock2 = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+sock2.bind((UDP_IP, UDP_PORT))
+
+sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+
+
+
+while True:
+    data, addr = sock2.recvfrom(1024) # buffer size is 1024 bytes
+    print "received message:", data
+
+sock.close()
+sock2.close()
 # initialize client socket
+
 	# does socket need to have different ports for receiving vs listening???
 
 # listen for user inputs (including no input)
