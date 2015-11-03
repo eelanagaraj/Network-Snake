43dbbea46cc739d794f07b6f097a3c8262765665
Eela <Eela1@Eelas-MacBook-Air.local>
2015-11-03 13:51:35 -0500

    file skeleton

--- client.py
+++ client.py
@@ -1,3 +1,17 @@
 #!/usr/bin/python
 
-" client-side code"
+""" client-side: listening for user inputs for snake,
+		send user inputs as UDP packets to server,
+		receives graphical rendering as UDP packets from server,
+		renders graphics for user"""
+
+import socket
+
+# initialize client socket
+	# does socket need to have different ports for receiving vs listening???
+
+# listen for user inputs (including no input)
+
+# send user inputs to server socket in UDP packets
+
+# receive UDP packets from server and render graphics for user
--- server.py
+++ server.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python
 
-""" server-side code"
+""" server-side: receive client's UDP packets of user inputs,
+		perform game computations on these inputs,
+		return entire graphical representation to client"""
+import socket
+
+# initialize socket
+
+# receive UDP packets on this socket
+
+# extract user data from UDP packets instead of event-listening
+
+# perform snake computations on this extracted data
+
+# send UDP packet containing graphical rendering to the client socket
