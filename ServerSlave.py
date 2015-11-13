a049a103b84d6a3b3a12b0011fa270f264fc7805
islandereleven <seanvedrinelle@college.harvard.edu>
2015-11-12 22:59:40 -0500

    Update ServerSlave.py

--- ServerSlave.py
+++ ServerSlave.py
@@ -48,6 +48,129 @@ def packet_handler(curr_seq_number, seq_number, number_payloads, payloads) :
         return (seq_number + 1, payloads)
         #OR if del is funky: return (seq_number + 1, payloads[:seq_number - curr_seq_number + 1])
         # see which is faster --> del may be better long run since modifies in place but idk
+        
+        
+        
+    #### STUFF TO BE transplanted to this function for the game to play
+    
+    """"# function to detect collisions serpent->serpent & serpent->apple
+	def collide(screen, x1, x2, y1, y2, w1, w2, h1, h2):
+	    if x1+w1>x2 and x1<x2+w2 and y1+h1>y2 and y1<y2+h2:
+	        #f=pygame.font.SysFont('Arial', 30);
+	        #t=f.render('collision', True, (0, 0, 0));
+	        #screen.blit(t, (10, 270));
+	        #pygame.display.update();
+	        #pygame.time.wait(200);
+	        return True
+	    else:
+	        return False
+	    
+	    
+	# stuff to do if you die
+	def die(screen, score):
+	    f=pygame.font.SysFont('Arial', 30);
+	    t=f.render('Your score was: '+str(score), True, (0, 0, 0));
+	    screen.blit(t, (10, 270));
+	    pygame.display.update();
+	    pygame.time.wait(2000);
+	    sys.exit(0)
+
+	print 'game started'
+    # server side gui stuff here need to share it with client
+    
+    # \/ initial snake block positions \/
+    xs = [290, 290, 290, 290, 290];
+    ys = [290, 270, 250, 230, 210];
+    
+    # number of frame per second the game can play at
+    fps = 1
+    
+    # \/ initial snake direction, score & position of the apple
+    dirs = 0;
+    score = 0;
+    
+    #\/ modification to see the snake eat the first apple
+    applepos = (330,270)#(random.randint(0, 590), random.randint(0, 590));
+    
+
+    ## GUI STUFF TO BE MOVED \/
+    # we start and customize the pygame gui
+    pygame.init();
+    s=pygame.display.set_mode((600, 600));
+    pygame.display.set_caption('Snake');
+    
+    # we create our apple and our snake blocks
+    block_size = (20, 20)
+    appleimage = pygame.Surface((block_size[0], block_size[1]));
+    appleimage.fill((0, 255, 0));
+    img = pygame.Surface((block_size[0], block_size[1]));
+    img.fill((255, 0, 0));
+    ## /\ /\ /\ /\
+    
+    # other stuff
+    f = pygame.font.SysFont('Arial', 20);
+    clock = pygame.time.Clock()
+    
+    while True:
+        # we run at one fps
+        clock.tick(fps)
+        
+        ## Following lines will be server side stuff
+        # if we have a command in our queue
+        if qi.qsize() > 0:
+            dirs = Q.get()
+        else:
+            pass
+        # REF 1*
+        i = len(xs)-1
+        
+        while i >= 2:
+            if collide(s, xs[0], xs[i], ys[0], ys[i], block_size[0], block_size[1],block_size[0], block_size[1]):
+                die(s, score)
+            i-= 1
+            
+    
+        if collide(s, xs[0], applepos[0], ys[0], applepos[1], 20, 10, 20, 10):
+            score+=1;
+            xs.append(700);
+            ys.append(700);
+            applepos=(random.randint(0,590),random.randint(0,590))
+        
+        # if we hit a wall -> death
+        if xs[0] < 0 or xs[0] > 580 or ys[0] < 0 or ys[0] > 580: 
+            die(s, score)
+        i = len(xs)-1
+        
+        # we update the position of the snake's body
+        while i >= 1:
+            xs[i] = xs[i-1];ys[i] = ys[i-1];i -= 1
+            
+        if dirs==0:
+            ys[0] += 20
+        elif dirs==1:
+            xs[0] += 20
+        elif dirs==2:
+            ys[0] -= 20
+        elif dirs==3:
+            xs[0] -= 20    
+        s.fill((255, 255, 255))
+
+       	guidict = dict()
+       	guidict['xs'] = xs
+        guidict['ys'] = ys
+        guidict['applepos'] = applepos
+        guidict['score'] = score
+
+        ## client side stuff: blit the snake, the apple & score
+        #snake
+        for i in range(0, len(xs)):
+            s.blit(img, (xs[i], ys[i]))
+        # apple
+        s.blit(appleimage, applepos);
+        # score
+        t=f.render("score:" + str(score), True, (0, 0, 0));
+        s.blit(t, (10, 10));
+        pygame.display.update()"""
 
 
 
