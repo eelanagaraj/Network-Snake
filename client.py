7788188129ef16a7a4cec4057347034950fad817
islandereleven <seanvedrinelle@college.harvard.edu>
2015-11-30 15:21:15 -0500

    Update client.py

--- client.py
+++ client.py
@@ -7,11 +7,46 @@
 
 import socket
 
+import socket
+import time
+import struct 
+
+
+def function5():
+	print 5
+## This Tcp wizardry sends timestamp to a server @ TCP_IP TCP_PORT waits delay seconds
+## and then executes stuff, here this is print 5
+def ClientMeister(Server_ip = '10.251.51.211', Server_port = 5005, delay = 4, funk = function5):
+	TCP_IP = '127.0.0.1'
+	TCP_IP = Server_ip
+
+	TCP_PORT = Server_port
+	BUFFER_SIZE = 256
+
+	packer = struct.Struct('d')
+	reftime = packer.pack(time.time()*1000)#"Hello, World!"
+
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((TCP_IP, TCP_PORT))
+	s.send(reftime)
+	data = s.recv(BUFFER_SIZE)
+	s.close()
+
+	startref = packer.unpack(reftime)
+
+	while (time.time() - delay)*1000 < startref[0]: pass
+
+	funk()
+
+ClientMeister()
 # some UDP packet sending experiments yay!
 My_IP = "10.251.54.255"
 Sean_IP = "10.251.54.174"
 UDP_IP = "10.251.48.230"
 UDP_PORT = 4000
+
+
+
 
 # note that data sent must be a string, though we can potentially use serializing libraries to send complex data objects
 	# my fave serializable python module is cPickle (pretty fast, straightforward to use) so we can look into that if need be
