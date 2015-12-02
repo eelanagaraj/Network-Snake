a959e71d2dfe5b7ed8ccb8fa0246d9303d986bde
Eela Nagaraj <eelanagaraj@college.harvard.edu>
2015-12-01 23:10:30 -0500

    indentation

--- helpers.py
+++ helpers.py
@@ -16,82 +16,76 @@ import ujson
 """
 class Stopwatch(object):
 	def __init__(self):
-        """Initialize a new `Stopwatch`, but do not start timing."""
-        self.start_time = None
-        self.stop_time = None
+		"""Initialize a new `Stopwatch`, but do not start timing."""
+		self.start_time = None
+		self.stop_time = None
 
 	def start(self):
-        """Start timing."""
-        self.start_time = time.time()
+		"""Start timing."""
+		self.start_time = time.time()
 
 	def stop(self):
-        """Stop timing."""
-        self.stop_time = time.time()
+		"""Stop timing."""
+		self.stop_time = time.time()
 
 	@property
 	def time_elapsed(self):
-        """Return the number of seconds that have elapsed since this
-        `Stopwatch` started timing.
-        This is used for checking how much time has elapsed while the timer is
-        still running.
-        """
-        assert not self.stop_time, \
-            "Can't check `time_elapsed` on an ended `Stopwatch`."
-        return time.time() - self.start_time
+		"""Return the number of seconds that have elapsed since this
+		`Stopwatch` started timing.
+		This is used for checking how much time has elapsed while the timer is
+		still running.
+		"""
+		assert not self.stop_time, \
+			"Can't check `time_elapsed` on an ended `Stopwatch`."
+		return time.time() - self.start_time
 
 	@property
 	def total_run_time(self):
-        """Return the number of seconds that elapsed from when this `Stopwatch`
-        started to when it ended.
-        """
-        return self.stop_time - self.start_time
+		"""Return the number of seconds that elapsed from when this `Stopwatch`
+		started to when it ended.
+		"""
+		return self.stop_time - self.start_time
 
 	def __enter__(self):
-        """Start timing and return this `Stopwatch` instance."""
-	self.start()
-        return self
+		"""Start timing and return this `Stopwatch` instance."""
+		self.start()
+		return self
 
 	def __exit__(self, type, value, traceback):
-        """Stop timing.
-        If there was an exception inside the `with` block, re-raise it.
-        >>> with Stopwatch() as stopwatch:
-        ...     raise Exception
-        Traceback (most recent call last):
-            ...
-        Exception
-        """
-        self.stop()
-        if type:
-            raise type, value, traceback
+		"""Stop timing.
+		If there was an exception inside the `with` block, re-raise it.
+		>>> with Stopwatch() as stopwatch:
+		...     raise Exception
+		Traceback (most recent call last):
+			...
+		Exception
+		"""
+		self.stop()
+		if type:
+			raise type, value, traceback
 
-Client_IP = "10.251.48.115"
-Server_IP = "10.251.59.41"
-Client_send_server_receive = 4000
-Server_send_client_receive = 4001
-
-        self.start()
-        return self
+		self.start()
+		return self
 
 	def __exit__(self, type, value, traceback):
-        """Stop timing.
-        If there was an exception inside the `with` block, re-raise it.
-        >>> with Stopwatch() as stopwatch:
-        ...     raise Exception
-        Traceback (most recent call last):
-            ...
-        Exception
-        """
-        self.stop()
-        if type:
-            raise type, value, traceback
-
-"""
+		"""Stop timing.
+		If there was an exception inside the `with` block, re-raise it.
+		>>> with Stopwatch() as stopwatch:
+		...     raise Exception
+		Traceback (most recent call last):
+			...
+		Exception
+		"""
+		self.stop()
+		if type:
+			raise type, value, traceback
 
 """ -assume data is of form: data = (seq_number, data_payload, prev_payloads)
 	-payloads is a list of all data payloads that are included, 
 		in decr sequence order (e.g. [recent, ..., oldest])
 	-return tuple of: (new_current_seq, [most recent payload, ... oldest]) 
 	**** maybe reverse within this function itself???? see what works best """
+	
 def packet_handler(curr_seq_number, seq_number, payloads) :
 	# check if current sequence number matches the sent packet
 	if (curr_seq_number == seq_number) :
