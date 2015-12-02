ccc4b4b64aeed65b57079a1b378136f54b8b3ca6
Matthew Carter <cartermatthewm@gmail.com>
2015-12-01 18:42:43 -0800

    timer added to helpers

--- helpers.py
+++ helpers.py
@@ -15,6 +15,40 @@ import cPickle as pkl
 	Sean IP
 """
 
+class Timer:
+    def __init__(self, func=time.perf_counter):
+        self.elapsed = 0.0
+        self._func = func
+        self._start = None
+
+    def start(self):
+        if self._start is not None:
+            raise RuntimeError('Already started')
+        self._start = self._func()
+
+    def stop(self):
+        if self._start is None:
+            raise RuntimeError('Not started')
+        end = self._func()
+        self.elapsed += end - self._start
+        self._start = None
+
+    def reset(self):
+        self.elapsed = 0.0
+
+    @property
+    def running(self):
+        return self._start is not None
+
+    def __enter__(self):
+        self.start()
+        return self
+
+    def __exit__(self, *args):
+        self.stop()
+
+
+
 """ -assume data is of form: data = (seq_number, data_payload, prev_payloads)
 	-payloads is a list of all data payloads that are included, 
 		in decr sequence order (e.g. [recent, ..., oldest])
