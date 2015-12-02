6a8f737c24efbec6bf2e5b9f9b64f5da7f97afe3
Eela Nagaraj <eelanagaraj@college.harvard.edu>
2015-12-01 20:09:39 -0500

    arg

--- network_analytics.py
+++ network_analytics.py
@@ -36,7 +36,7 @@ def count_received (expected_volume, match_data, receive_IP, receive_port):
 	print "packets recceived :", packet_count
 	print "percent received :", packet_count/float(expected_volume)
 
-receiver_IP = "10.251.55.177"
+receiver_IP = "10.251.48.115"
 
 packet = helpers.serializer(14, [1,0,3]) 
 machine = int(raw_input("Sender machine press 0, receiver machine press 1"))
